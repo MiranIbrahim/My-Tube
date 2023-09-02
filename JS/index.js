5506b33199e69f5479db1c2fcafd2ba2e9c9f523
Miran Ibrahim <moeenuddin.miran@gmail.com>
2023-09-02 16:20:46 +0600

    almost 85% done

--- JS/index.js
+++ JS/index.js
@@ -4,75 +4,94 @@ const loadApiData = async () => {
   );
   const data = await response.json();
 
-  // console.log(data);
-
   const categoryContainer = document.getElementById("category-container");
 
   data.data.forEach((category) => {
     const div = document.createElement("div");
     div.innerHTML = `
-
-            <button onclick="loadContent('${category.category_id}')" class="rounded-md bg-gray-300 px-5 py-2">${category.category}</button>
-        `;
+    <button id="btn-color" class="rounded-md bg-gray-300 px-5 py-2" onclick="loadContent('${category.category_id}', this)">${category.category}</button>      
+    `;
 
     categoryContainer.appendChild(div);
-
-    loadContent(category);
   });
 };
 
-const loadContent = async (categoryID) => {
+const loadContent = async (categoryID, button) => {
   const response = await fetch(
     `https://openapi.programming-hero.com/api/videos/category/${categoryID}`
   );
   const data = await response.json();
-  
-  const boxContainer = document.getElementById('box-container');
-  
-boxContainer.innerHTML = '';
-  
-//   console.log(data);
-data.data?.forEach((content)=>{
-    
-    const div = document.createElement("div");  
+  const boxContainer = document.getElementById("box-container");
+
+  boxContainer.innerHTML = "";
+
+  if (data.data.length == 0) {
+    boxContainer.classList.remove("grid")
+    boxContainer.innerHTML = `
+    <div class="text-center mt-24">
+        <div class="mb-8">
+            <img src="./images/Icon.png" alt="" class="mx-auto">
+        </div>
+        <div class="text-2xl font-bold">
+            Oops!! Sorry, There is no content here
+        </div>
+    </div>
+    `;
+  }
+  else{
+    boxContainer.classList.add("grid")
+  }
+  data.data?.forEach((content) => {
+    const div = document.createElement("div");
     div.innerHTML = `
-    <div class="card w-96 bg-base-100">
-          <figure class="rounded-lg" >
-            <img
-              src=${content.thumbnail}
-            />
-          </figure>
-          <div class=" mt-5">
-            <div class="flex gap-3">
-              <div class="avatar">
-                <div class="w-12 h-12 rounded-full">
-                  <img
-                    src=${content.authors[0].profile_picture}
-                  />
+      <div class="card">
+        <figure class="rounded-lg h-44">
+          <img src=${content.thumbnail} />
+        </figure>
+        <div class=" mt-5">
+          <div class="flex gap-3">
+            <div class="avatar">
+              <div class="w-12 h-12 rounded-full">
+                <img src=${content.authors[0].profile_picture} />
+              </div>
+            </div>
+            <div class="">
+              <h2 class="card-title">
+                ${content.title}
+              </h2>
+              <div class="flex gap-3 items-center">
+                <p class="font-normal text-gray-400 items-center">
+                  ${content.authors[0].profile_name}
+                </p>
+                <div>
+                  ${
+                    content.authors[0].verified
+                      ? `<img src="./images/verified.png" class="w-5 h-5" />`
+                      : ""
+                  }
                 </div>
               </div>
-              <div class="">
-                <h2 class="card-title">
-                ${content.title}
-                </h2>
-                <p class="font-normal text-gray-400 flex gap-3 items-center">
-                ${content.authors[0].profile_name}
-                  <img src="./images/verified.png" alt="" class="w-5 h-5" />
-                </p>
-                <p class="font-normal text-gray-400 flex gap-3 items-center">
+              <p class="font-normal text-gray-400 items-center">
                 ${content.others.views}
-                </p>
-              </div>
+              </p>
             </div>
           </div>
         </div>
+      </div>
     `;
     boxContainer.appendChild(div);
-});
-  
+  });
+
+  const buttons = document.querySelectorAll("#btn-color");
+
+  buttons.forEach((btn) => {
+    btn.classList.remove("bg-red-500", "text-white");
+    btn.classList.add("bg-gray-300", "text-black");
+  });
+
+  button.classList.remove("bg-gray-300", "text-black");
+  button.classList.add("bg-red-500", "text-white");
 };
-
-
 
 loadApiData();
 loadContent("1000");
